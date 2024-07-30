23559782c29056c66a6d12f4f4a147e340c4d40e
harshams22 <harshamsheshadri@gmail.com>
2024-07-30 22:45:03 +0530

    final try

--- .github/workflows/ascii.sh
+++ .github/workflows/ascii.sh
@@ -3,5 +3,4 @@ sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, i am a DRAGON...RRR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
-cd .github/workflows/ascii.sh
 ls -ltra
